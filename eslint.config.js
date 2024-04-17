af2d31d2dbfc3d3e0641ed6e4beb6ebfa97dae87
Yugang Cao <1442770751@qq.com>
2024-04-17 18:03:13 +0800

    feat: update deps (#29)

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,22 @@
-const reactRecommended = require('eslint-plugin-react/configs/recommended');
-const markdown = require("eslint-plugin-markdown");
-module.exports = {
+import react from 'eslint-plugin-react';
+import globals from 'globals'
+import markdown from "eslint-plugin-markdown";
+export default [
     ...markdown.configs.recommended,
-    ...reactRecommended
-}
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        plugins: {
+            react,
+        },
+        languageOptions: {
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+            globals: {
+                ...globals.browser,
+            },
+        }
+    }
+]
